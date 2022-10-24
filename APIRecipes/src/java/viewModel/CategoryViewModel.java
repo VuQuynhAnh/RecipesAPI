cf99926bd9643209132d9fadc804796721600839
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2022-10-24 15:21:05 +0700

    update Category View Model

--- APIRecipes/src/java/viewModel/CategoryViewModel.java
+++ APIRecipes/src/java/viewModel/CategoryViewModel.java
@@ -5,6 +5,7 @@
  */
 package viewModel;
 
+import entity.Category;
 import java.io.Serializable;
 import java.sql.Date;
 
@@ -12,87 +13,18 @@ import java.sql.Date;
  *
  * @author DELL
  */
-public class CategoryViewModel implements Serializable {
+public class CategoryViewModel extends Category implements Serializable {
 
-    private int id;
-    private String name;
-    private int status;
-    private Date createDate;
-    private int createUser;
-    private Date updateDate;
-    private int updateUser;
     private String createUserDisplay;
     private String updateUserDisplay;
 
     public CategoryViewModel(int id, String name, int status, Date createDate, int createUser, Date updateDate, int updateUser, String createUserDisplay, String updateUserDisplay) {
-        this.id = id;
-        this.name = name;
-        this.status = status;
-        this.createDate = createDate;
-        this.createUser = createUser;
-        this.updateDate = updateDate;
-        this.updateUser = updateUser;
+        super(id, name, status, createDate, createUser, updateDate, updateUser);
         this.createUserDisplay = createUserDisplay;
         this.updateUserDisplay = updateUserDisplay;
     }
 
     public CategoryViewModel() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public int getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(int createUser) {
-        this.createUser = createUser;
-    }
-
-    public Date getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public int getUpdateUser() {
-        return updateUser;
-    }
-
-    public void setUpdateUser(int updateUser) {
-        this.updateUser = updateUser;
     }
 
     public String getCreateUserDisplay() {
