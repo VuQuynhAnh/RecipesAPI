9cd1450af39e7f4314ca78ed93295245057967b9
anh.vu3@sotatek.com <anh.vu3@sotatek.com>
2022-11-11 19:27:25 +0700

    Notification API

--- APIRecipes/src/java/services/NotificationService.java
+++ APIRecipes/src/java/services/NotificationService.java
@@ -5,28 +5,19 @@
  */
 package services;
 
-import common.FolderNameConstant;
-import dao.CategoryDao;
 import dao.NotificationDao;
-import dao.UploadImageDao;
 import dao.UsersDao;
-import entity.Category;
 import entity.Notifications;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import javax.ejb.Stateless;
-import javax.management.Notification;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import viewModel.CategoryViewModel;
 import viewModel.NotificationViewModel;
 
 /**
@@ -37,76 +28,93 @@ import viewModel.NotificationViewModel;
 @Path("/notification")
 public class NotificationService {
 
-    NotificationDao categoryDao = null;
+    NotificationDao notificationDao = null;
     UsersDao usersDao = null;
 
     public NotificationService() {
-        categoryDao = new NotificationDao();
+        notificationDao = new NotificationDao();
         usersDao = new UsersDao();
     }
 
-//    @GET
-//    @Path("getAll")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<NotificationViewModel> getCategories() {
-//        return categoryDao.getData();
-//    }
-//
-//    @GET
-//    @Path("getLists")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<NotificationViewModel> getCategories(
-//            @QueryParam("keyword") String keyword,
-//            @QueryParam("isGetAll") boolean isGetAll,
-//            @QueryParam("sortIdDESC") boolean sortIdDESC,
-//            @QueryParam("sortNameASC") boolean sortNameASC,
-//            @QueryParam("sortTotalRecipeDESC") boolean sortTotalRecipeDESC,
-//            @QueryParam("pageIndex") int pageIndex,
-//            @QueryParam("pageSize") int pageSize
-//    ) {
-//        return categoryDao.getData(keyword, isGetAll, sortIdDESC, sortNameASC, sortTotalRecipeDESC, pageIndex, pageSize);
-//    }
-//
-//    @GET
-//    @Path("detail")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public NotificationViewModel getNotificationById(@QueryParam("id") int id) {
-//        return categoryDao.getDataById(id);
-//    }
-//
-//    @POST
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public String insert(Notifications noti) {
-//
-//        // validate
-//        if (noti.getDescription().trim().length() == 0) {
-//            return "Notification description is requied!";
-//        } else if (!usersDao.checkExistUser(noti.getCreateUser())) {
-//            return "Notification createUser with id = " + noti.getCreateUser() + " is not exist or deleted!";
-//        } else if (!usersDao.checkExistUser(noti.getNotificationId())) {
-//            return "Notification notificationId with id = " + noti.getCreateUser() + " is not exist or deleted!";
-//        } else if (categoryDao.insertData(noti)) {
-//            return "Success!";
-//        }
-//        return "Failed!";
-//    }
-//
-//    @DELETE
-//    @Path("delete")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Consumes(MediaType.TEXT_PLAIN)
-//    public String delete(@QueryParam("id") int id, @QueryParam("updateUser") int deleteId) {
-//        if (!categoryDao.checkExistCategory(id)) {
-//            return "Category width id = " + id + " is not exist or deleted!";
-//        } else if (!usersDao.checkExistUser(deleteId)) {
-//            return "Category updateUser with id = " + deleteId + " is not exist or deleted!";
-//        } else if (!usersDao.isUserAdmin(deleteId)) {
-//            return "Category updateUser with id = " + deleteId + " is not a admin!";
-//        } else if (categoryDao.deleteData(id, deleteId)) {
-//            return "Success!";
-//        }
-//        return "Failed!";
-//    }
+    @GET
+    @Path("getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<NotificationViewModel> getCategories() {
+        return notificationDao.getData();
+    }
+
+    @GET
+    @Path("getNotificationByUserId")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<NotificationViewModel> getNotificationByUserId(@QueryParam("userId") int userId, @QueryParam("status") int status) {
+        return notificationDao.getNotificationByUserId(userId, status);
+    }
+
+    @GET
+    @Path("getNotificationByCreateUserId")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<NotificationViewModel> getNotificationByCreateUserId(@QueryParam("createUserId") int createUserId, @QueryParam("status") int status) {
+        return notificationDao.getNotificationByCreateUserId(createUserId, status);
+    }
+
+    @GET
+    @Path("detail")
+    @Produces(MediaType.APPLICATION_JSON)
+    public NotificationViewModel getNotificationById(@QueryParam("id") int id) {
+        return notificationDao.getDataById(id);
+    }
+
+    @GET
+    @Path("readNotification")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String readNotification(@QueryParam("id") int id) {
+        if (!notificationDao.checkExistNotification(id)) {
+            return "Notification width id = " + id + " is not exist or deleted!";
+        } else if (notificationDao.readNotification(id)) {
+            return "Success!";
+        }
+        return "Failed!";
+    }
+
+    @GET
+    @Path("unReadNotification")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String unReadNotification(@QueryParam("id") int id) {
+        if (!notificationDao.checkExistNotification(id)) {
+            return "Notification width id = " + id + " is not exist or deleted!";
+        } else if (notificationDao.unReadNotification(id)) {
+            return "Success!";
+        }
+        return "Failed!";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String insert(Notifications notification) {
+
+        // validate
+        if (notification.getDescription().trim().length() == 0) {
+            return "Notification description is requied!";
+        } else if (!usersDao.checkExistUser(notification.getCreateUser())) {
+            return "Notification createUser with id = " + notification.getCreateUser() + " is not exist or deleted!";
+        } else if (notificationDao.insertData(notification)) {
+            return "Success!";
+        }
+        return "Failed!";
+    }
+
+    @DELETE
+    @Path("delete")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String delete(@QueryParam("id") int id) {
+        if (!notificationDao.checkExistNotification(id)) {
+            return "Notification width id = " + id + " is not exist or deleted!";
+        } else if (notificationDao.deleteNotification(id)) {
+            return "Success!";
+        }
+        return "Failed!";
+    }
 
 }
